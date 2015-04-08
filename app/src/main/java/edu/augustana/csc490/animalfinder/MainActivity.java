55de6026e187fcdc53ad5e716ac51e9e30b4faf2
ashearouse <Swimmerboy08>
2015-04-08 11:03:32 -0500

    base version functioning

--- app/src/main/java/edu/augustana/csc490/animalfinder/MainActivity.java
+++ app/src/main/java/edu/augustana/csc490/animalfinder/MainActivity.java
@@ -4,46 +4,64 @@ import android.app.Activity;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static java.security.AccessController.getContext;
 
 
 public class MainActivity extends Activity {
     ImageView mainImage;
+    TextView foundBox;
+    int trackX = 0;
+    int trackY = 0;
     int x1, x2;
     int y1, y2;
+    int diffX, diffY;
+    int density;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         //comment
         //lets test this!
+        setImageListener();
+        //setClickListener();
+    }
+
+    private void setImageListener(){
         mainImage = (ImageView) findViewById(R.id.mainImage);
         mainImage.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 int action = MotionEventCompat.getActionMasked(event);
 
+
                 switch (action) {
                     case (MotionEvent.ACTION_DOWN):
                         Log.d("DEBUG_TAG", "Action was DOWN");
-                        x1 = (int) event.getX();
-                        y1 = (int) event.getY();
+                        capturePress(event);
                         return true;
                     case (MotionEvent.ACTION_MOVE):
                         Log.d("DEBUG_TAG", "Action was MOVE");
-                        x2 = (int) event.getX();
-                        y2 = (int) event.getY();
-                        mainImage.scrollBy( x1-x2,y1-y2);
+
                         return true;
                     case (MotionEvent.ACTION_UP):
                         Log.d("DEBUG_TAG", "Action was UP");
+                        captureRelease(event);
                         return true;
                     case (MotionEvent.ACTION_CANCEL):
                         Log.d("DEBUG_TAG", "Action was CANCEL");
@@ -57,6 +75,50 @@ public class MainActivity extends Activity {
                 }
             }
         });
+    }
+
+    private void captureRelease(MotionEvent event){
+        x2 = (int) event.getX();
+        y2 = (int) event.getY();
+        diffX = x1-x2;
+        diffY = y1-y2;
+        mainImage.scrollBy(x1-x2,y1-y2);
+        trackX = trackX + diffX;
+        trackY = trackY + diffY;
+    }
+
+    private void capturePress(MotionEvent event){
+        x1 = (int) event.getX();
+        y1 = (int) event.getY();
+        DisplayMetrics metrics2 = getResources().getDisplayMetrics();
+        density = metrics2.densityDpi;
+        Toast toast = Toast.makeText(getApplicationContext(), "x: " + (x1 + trackX) + " y: " + (y1 + trackY),Toast.LENGTH_SHORT);
+        toast.show();
+        if((trackX + x1) / (density / 160) >= 725 && (trackX + x1) / (density / 160) <= 762 && (trackY + y1) / (density / 160) >= 325 && (trackY + y1) / (density / 160) <= 356){
+            Toast toast2 = Toast.makeText(getApplicationContext(), "You won!", Toast.LENGTH_SHORT);
+            toast2.show();
+        }
+
+    }
+
+
+
+    private void setClickListener(){
+        foundBox = (TextView) findViewById(R.id.foundPanda);
+        foundBox.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Toast toast = Toast.makeText(getApplicationContext(), "You won!", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+        );
+    }
+
+    private void showWin(){
+        Toast toast = Toast.makeText(getApplicationContext(), "You won!", Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     private View.OnDragListener imageDragListener = new View.OnDragListener() {
